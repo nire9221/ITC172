5e65d092225fdfab12f09d5726ea709bf3036046
nire9221 <[nire9221@gmail.com]>
2020-01-14 12:27:32 -0800

    itc172-pythonclub update

--- PythonClub/PythonClub/Club/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    output = "Jinyoung"
-    return HttpResponse(output)
--- pythonclub/pythonclub/urls.py
+++ pythonclub/pythonclub/urls.py
@@ -1,4 +1,4 @@
-"""web_project URL Configuration
+"""pythonclub URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
