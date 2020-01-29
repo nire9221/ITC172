0d1c9969c8ed99ef1f4949e0bd957b99862f1c9c
erin <nire9221@gmail.com>
2020-01-29 13:19:44 -0800

    updated

--- pythonclub/clubApp/urls.py
+++ pythonclub/clubApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
+
 ]
--- pythonclub/clubApp/views.py
+++ pythonclub/clubApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Meeting, Meeting_Minutes, Resource, Event
+
 
 # Create your views here.
-def index (request):
+def index(request):
     return render(request, 'clubapp/index.html')
+
+
+def gettypes(request):
+    type_list = meetingType.objects.all()
+    return render(request, 'clubApp/types.html', {'type_list': type_list})
