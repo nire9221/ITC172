f2b42a0904daaac279d65a77600a94586b12f4ac
erin <nire9221@gmail.com>
2020-01-29 13:41:23 -0800

    fixed error

--- pythonclub/clubApp/views.py
+++ pythonclub/clubApp/views.py
@@ -4,9 +4,9 @@ from .models import Meeting, Meeting_Minutes, Resource, Event
 
 # Create your views here.
 def index(request):
-    return render(request, 'clubapp/index.html')
+    return render(request, 'clubApp/index.html')
 
 
 def gettypes(request):
-    type_list = meetingType.objects.all()
+    type_list = Meeting.objects.all()
     return render(request, 'clubApp/types.html', {'type_list': type_list})
