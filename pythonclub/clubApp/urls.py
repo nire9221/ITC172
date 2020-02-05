345224ad317d3946342ed184987167ee76dfe6f3
erin <nire9221@gmail.com>
2020-02-05 13:49:44 -0800

    udated

--- pythonclub/clubApp/urls.py
+++ pythonclub/clubApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
-
+    path('getminutes/', views.getminutes, name='minute'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='meetingdetail'),
 ]
--- pythonclub/clubApp/views.py
+++ pythonclub/clubApp/views.py
@@ -10,3 +10,21 @@ def index(request):
 def gettypes(request):
     type_list = Meeting.objects.all()
     return render(request, 'clubApp/types.html', {'type_list': type_list})
+
+
+def getminutes(request):
+    minute_info = Meeting_Minutes.objects.all()
+    return render(request, 'clubApp/minute.html', {'minute_info': minute_info})
+
+
+def meetingdetail(request, id):
+    meetingname = get_object_or_404(gettypes, pk=id)
+    meetingtime = meetingname.getminutes
+
+    context = {
+        'meetingname': meetingname,
+        'meetingtime': meetingtime,
+
+
+    }
+    return render(request, 'clubApp/meetingdetail.html', context=context)
